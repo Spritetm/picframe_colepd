5b7bae0c8d2eb6bfe91196088de9443707a980da
Jeroen Domburg <git@j0h.nl>
2022-07-31 14:47:57 +0800

    cleanup

--- www/conv/conv.c
+++ www/conv/conv.c
@@ -4,6 +4,8 @@
 #include <time.h>
 #include "gd.h"
 #include <math.h>
+#include <assert.h>
+#include <string.h>
 
 #define EPD_W 600
 #define EPD_H 448
@@ -13,22 +15,40 @@ gdImagePtr load_scaled(char *filename) {
 	f=fopen(filename, "r");
 	if (f==NULL) {
 		perror(filename);
-		exit(1);
+		return NULL;
 	}
 	gdImagePtr oim=gdImageCreateFromJpegEx(f, 1);
-	gdImagePtr nim=gdImageCreateTrueColor(EPD_W, EPD_H);
-	int bgnd = gdImageColorAllocate(nim, 255,255,255);
-	gdImageFilledRectangle(nim, 0, 0, EPD_W, EPD_H, bgnd);
-	int nw=EPD_W;
-	int nh=(gdImageSY(oim)*EPD_W)/gdImageSX(oim);
-	if (nh>EPD_H) {
-		nh=EPD_H;
-		nw=(gdImageSX(oim)*EPD_H)/gdImageSY(oim);
+	if (oim==NULL) {
+		//try as png?
+		oim=gdImageCreateFromPng(f);
+		if (oim==NULL) {
+			return NULL;
+		}
 	}
-	gdImageCopyResampled(nim, oim, (EPD_W-nw)/2, (EPD_H-nh)/2, 
-				0, 0, nw, nh, gdImageSX(oim), gdImageSY(oim));
-	printf("Resized to %d, %d\n", nw, nh);
-	gdImageDestroy(oim);
+
+	gdImagePtr nim=NULL;
+	if (gdImageSY(oim)==EPD_H && gdImageSX(oim)==EPD_W && gdImageTrueColor(oim)) {
+		//no scaling needed
+		nim=oim;
+	} else {
+		//Need scaling or converting to truecolor
+		nim=gdImageCreateTrueColor(EPD_W, EPD_H);
+		if (nim==NULL) {
+			gdImageDestroy(oim);
+			return NULL;
+		}
+		int bgnd = gdImageColorAllocate(nim, 255,255,255);
+		gdImageFilledRectangle(nim, 0, 0, EPD_W, EPD_H, bgnd);
+		int nw=EPD_W;
+		int nh=(gdImageSY(oim)*EPD_W)/gdImageSX(oim);
+		if (nh>EPD_H) {
+			nh=EPD_H;
+			nw=(gdImageSX(oim)*EPD_H)/gdImageSY(oim);
+		}
+		gdImageCopyResampled(nim, oim, (EPD_W-nw)/2, (EPD_H-nh)/2, 
+					0, 0, nw, nh, gdImageSX(oim), gdImageSY(oim));
+		gdImageDestroy(oim);
+	}
 	return nim;
 }
 
@@ -50,9 +70,9 @@ float gamma_linear(float in) {
 
 //note: takes linearized RGB
 void rgb_to_lab(float *rgb, float *lab) {
-    float var_R = rgb[0] * 100;
-    float var_G = rgb[1] * 100;
-    float var_B = rgb[2] * 100;
+	float var_R = rgb[0] * 100;
+	float var_G = rgb[1] * 100;
+	float var_B = rgb[2] * 100;
 	if (var_R>100) var_R=100;
 	if (var_G>100) var_G=100;
 	if (var_B>100) var_B=100;
@@ -60,33 +80,33 @@ void rgb_to_lab(float *rgb, float *lab) {
 	if (var_G<0) var_G=0;
 	if (var_B<0) var_B=0;
 
-    //Observer. = 2°, Illuminant = D65
-    float inX = var_R * 0.4124f + var_G * 0.3576f + var_B * 0.1805f;
-    float inY = var_R * 0.2126f + var_G * 0.7152f + var_B * 0.0722f;
-    float inZ = var_R * 0.0193f + var_G * 0.1192f + var_B * 0.9505f;
+	//Observer. = 2°, Illuminant = D65
+	float inX = var_R * 0.4124f + var_G * 0.3576f + var_B * 0.1805f;
+	float inY = var_R * 0.2126f + var_G * 0.7152f + var_B * 0.0722f;
+	float inZ = var_R * 0.0193f + var_G * 0.1192f + var_B * 0.9505f;
 
-    float var_X = (inX / 95.047);
-    float var_Y = (inY / 100.0);
-    float var_Z = (inZ / 108.883);
+	float var_X = (inX / 95.047);
+	float var_Y = (inY / 100.0);
+	float var_Z = (inZ / 108.883);
 
-    if ( var_X > 0.008856 ) 
-        var_X = powf(var_X , ( 1.0f/3 )); 
-    else 
-        var_X = ( 7.787 * var_X ) + ( 16.0f/116 );
+	if ( var_X > 0.008856 ) 
+		var_X = powf(var_X , ( 1.0f/3 )); 
+	else 
+		var_X = ( 7.787 * var_X ) + ( 16.0f/116 );
 
-    if ( var_Y > 0.008856 )
-        var_Y = powf(var_Y , ( 1.0f/3 )); 
-    else
-        var_Y = ( 7.787 * var_Y ) + ( 16.0f/116 );
+	if ( var_Y > 0.008856 )
+		var_Y = powf(var_Y , ( 1.0f/3 )); 
+	else
+		var_Y = ( 7.787 * var_Y ) + ( 16.0f/116 );
 
-    if ( var_Z > 0.008856 )
-        var_Z = powf(var_Z , ( 1.0f/3 )); 
-    else 
-        var_Z = ( 7.787 * var_Z ) + ( 16.0f/116 );
+	if ( var_Z > 0.008856 )
+		var_Z = powf(var_Z , ( 1.0f/3 )); 
+	else 
+		var_Z = ( 7.787 * var_Z ) + ( 16.0f/116 );
 
-    lab[0] = ( 116 * var_Y ) - 16;
-    lab[1] = 500 * ( var_X - var_Y );
-    lab[2] = 200 * ( var_Y - var_Z );
+	lab[0] = ( 116 * var_Y ) - 16;
+	lab[1] = 500 * ( var_X - var_Y );
+	lab[2] = 200 * ( var_Y - var_Z );
 }
 
 void dist_diff(float *rgb, int x, int y, int i, float dif) {
@@ -118,22 +138,43 @@ float col_diff(float *c1, float *c2) {
 }
 
 int main(int argc, char **argv) {
-	gdImagePtr im=load_scaled(argv[1]);
+	char *im_in="";
+	char *im_out="";
+	char *bin_out="";
+	int error=0;
+	for (int i=1; i<argc; i++) {
+		if (strcmp(argv[i], "-p")==0 && i<argc-1) {
+			if (im_out[0]!=0) error=1;
+			i++;
+			im_out=argv[i];
+		} else if (strcmp(argv[i], "-o")==0 && i<argc-1) {
+			if (bin_out[0]!=0) error=1;
+			i++;
+			bin_out=argv[i];
+		} else {
+			if (im_in[0]!=0) error=1;
+			im_in=argv[i];
+		}
+	}
+	if (im_in[0]==0 || error) {
+		printf("Usage: %s [-o outfile.bin] [-p preview.png] infile.[jpg|png]\n", argv[0]);
+		printf("infile can be png or jpeg, if no outfile is given, output will go to stdout\n");
+		exit(error);
+	}
+
+	//Load image
+	gdImagePtr im=load_scaled(im_in);
+	if (!im) {
+		fprintf(stderr, "Could not load image %s\n", im_in);
+		exit(1);
+	}
+
 	flash_image_t *bin=calloc(sizeof(flash_image_t), 1);
+	assert(bin);
 	bin->hdr.id=0xfafa1a1a;
 	bin->hdr.timestamp=time(NULL);
 	
-/*
-	float epd_colors[7][3]={ //rgb
-		{0,0,0},
-		{1,1,1},
-		{0,1,0},
-		{0,0,1},
-		{1,0,0},
-		{1,1,0},
-		{1,0.6,0}
-	};
-*/
+	//RGB colors as displayed on the screen
 	float epd_colors[7][3]={ //rgb
 		{0,0,0},
 		{1,1,1},
@@ -145,6 +186,7 @@ int main(int argc, char **argv) {
 		{0.89, 0.51, 0.14}
 	};
 
+	//Convert float RGB colors to int colors
 	int epd_colors_int[7];
 	for (int i=0; i<7; i++) {
 		int c=0;
@@ -156,6 +198,7 @@ int main(int argc, char **argv) {
 	}
 
 	float *rgb=calloc(EPD_W*EPD_H*3, sizeof(float));
+	assert(rgb);
 	float *rgbp=rgb;
 	for (int y=0; y<EPD_H; y++) {
 		for (int x=0; x<EPD_W; x++) {
@@ -167,6 +210,7 @@ int main(int argc, char **argv) {
 	}
 
 	gdImagePtr tim=gdImageCreateTrueColor(EPD_W, EPD_H);
+	assert(tim);
 	for (int y=0; y<EPD_H; y++) {
 		int ob=0;
 		for (int x=0; x<EPD_W; x++) {
@@ -198,11 +242,27 @@ int main(int argc, char **argv) {
 			gdImageSetPixel(tim, x, y, epd_colors_int[best]);
 		}
 	}
-	FILE *of=fopen("out.png", "w");
-	gdImagePng(tim, of);
-	fclose(of);
-	of=fopen("epd-img.bin", "w");
-	fwrite(bin, sizeof(flash_image_t)-sizeof(bin->padding), 1, of);
-	fclose(of);
+
+	if (im_out[0]) {
+		FILE *of=fopen(im_out, "w");
+		if (!of) {
+			perror(im_out);
+			exit(1);
+		}
+		gdImagePng(tim, of);
+		fclose(of);
+	}
+	gdImageDestroy(tim);
+	if (bin_out[0]) {
+		FILE *of=fopen(bin_out, "wb");
+		if (!of) {
+			perror(bin_out);
+			exit(1);
+		}
+		fwrite(bin, sizeof(flash_image_t)-sizeof(bin->padding), 1, of);
+		fclose(of);
+	} else {
+		fwrite(bin, sizeof(flash_image_t)-sizeof(bin->padding), 1, stdout);
+	}
 }
 
