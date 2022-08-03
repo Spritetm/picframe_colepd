1834685440a00c5a83c8151425b58d974c4fbcb6
Sprite_tm <jeroen@spritesmods.com>
2022-08-03 11:57:50 +0800

    Add comments, fix int return vals

--- www/epd-info.php
+++ www/epd-info.php
@@ -22,26 +22,28 @@ function get_app_image_data($file) {
 
 $mysqli = mysqli_connect("localhost",$username, $pass, $db); 
 
-//Find device based on MAC
+//Sanitize MAC
 $mac="000000000000";
 if (isset($_GET["mac"])) $mac=$_GET["mac"];
 if (!preg_match("/^[0-9a-fA-F]{12}$/", $mac)) {
 	$mac="000000000000";
 }
 
-$result = $mysqli->query("SELECT id,tz,fw_upd FROM devices WHERE `mac`='$mac'");
+//Find device based on MAC
+$result = $mysqli->query("SELECT id,tz,fw_upd,update_hour FROM devices WHERE `mac`='$mac'");
 $dev_info=$result->fetch_assoc();
 if (!$dev_info) {
-	$stmt = $mysqli->prepare("INSERT INTO devices (mac,tz,fw_upd) VALUES (?,?,?)");
+	//No data... make a new one
+	$stmt = $mysqli->prepare("INSERT INTO devices (mac,tz,fw_upd,update_hour) VALUES (?,?,?,?)");
 	$tz="CST-8";
 	$fw_upd="picframe.bin";
-	$stmt->bind_param("sss", $mac, $tz, $fw_upd);
+	$update_hour=3;
+	$stmt->bind_param("sssi", $mac, $tz, $fw_upd, $update_hour);
 	$stmt->execute() || die($stmt->error);
-	//re-run query
-	$result = $mysqli->query("SELECT id,tz,fw_upd FROM devices WHERE `mac`='$mac'");
+	//re-run query to fetch the data... lazy, I know
+	$result = $mysqli->query("SELECT id,tz,fw_upd,update_hour FROM devices WHERE `mac`='$mac'");
 	$dev_info=$result->fetch_assoc();
 }
-
 
 //Insert checkin data into db
 $stmt = $mysqli->prepare("INSERT INTO checkins (device_id,battery_mv,ip) VALUES (?,?,?)");
@@ -59,16 +61,19 @@ $result = $mysqli->query("SELECT id FROM images ORDER BY timestamp DESC LIMIT 10
 //Get IDs of last 10 images
 for ($i=0; $i<10; $i++) {
 	$row=$result->fetch_assoc();
-	$image_ids[$i]=$row["id"];
+	$image_ids[$i]=intval($row["id"]);
 }
 
+//Dump all info in json
 $ret=array();
 $ret["time"]=time();
 $ret["fw_sha"]=get_app_image_data($dev_info["fw_upd"]);
 $ret["fw_upd"]=$dev_info["fw_upd"];
 $ret["tz"]=$dev_info["tz"];
+$ret["update_hour"]=intval($dev_info["update_hour"]);
 $ret["images"]=$image_ids;
 
+//send
 header("Content-Type: text/json");
 echo json_encode($ret);
 
